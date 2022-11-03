ff280193e8b270e35c0c315652fc0350f98bf2eb
nabilweq <nabeeltkanr@gmail.com>
2022-11-03 18:44:52 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+const express = require('express');
+//const connectDB = require('./db');
+
+const app = express();
+
+// Connect Database
+//connectDB();
+
+// Init Middleware
+app.use(express.json());
+
+// To remove CROS (cross-resource-origin-platform) problem
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*"); // to allow all client we use *
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "OPTIONS,GET,POST,PUT,PATCH,DELETE"
+    ); //these are the allowed methods
+    res.setHeader("Access-Control-Allow-Headers", "*"); // allowed headers (Auth for extra data related to authoriaztion)
+    next();
+});
+
+app.get('/', (req, res, ) => {
+    res.send('Backend of Health app is Working Fine.');
+});
+
+//Auth routes
+app.use('/api/auth', require('./routes/auth'));
+
+app.use('/api', require('./routes/index'))
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
